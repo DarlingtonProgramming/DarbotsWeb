a2d8e7f431aadfbb9ec93502c7a3671df1f54e41
年迈的老秋风Windy <cxcyhdyx@sina.com>
2018-11-12 20:33:36 -0500

    Remove CN Language

--- dynamicData/indexslides.js
+++ dynamicData/indexslides.js
@@ -1,66 +1,15 @@
 var XSYD_IndexSlides = {
     slides: [
         {
-            backgroundImage: "static/slidesResources/slide3.jpg",
-            cn:{
-                title: '<span class="text-black">Readin上线!</span>',
-                description: '<span class="text-black">由迪莫开发的Readin应用现在已经可以在Microsoft商店中下载</span>',
-                disableArticleButton: true,
-                customButton: true,
-                customButtonURL: "https://www.microsoft.com/zh-cn/p/readin/9nx3jtgvpl27?rtc=1/",
-                customButtonText: "Microsoft商店",
-                articleIDRelated: -1
-            },
-            en:{
-                title: '<span class="text-black">Readin is now available!</span>',
-                description: '<span class="text-black">Our APP Readin developed by Dimo Jang is now availble in Microsoft Store</span>',
-                disableArticleButton: true,
-                customButton: true,
-                customButtonURL: "https://www.microsoft.com/en-us/p/readin/9nx3jtgvpl27?rtc=1/",
-                customButtonText: "Microsoft Store",
-                articleIDRelated: -1
-            }
-        },
-        {
-            backgroundImage: "static/slidesResources/slide2.jpg",
-            cn:{
-                title: '<span class="text-black">形随意动招新!</span>',
-                description: '<span class="text-black">我们已经准备好在2019年迎接新人了!</span>',
-                disableArticleButton: false,
-                customButton: false,
-                customButtonURL: null,
-                customButtonText: null,
-                articleIDRelated: 2
-            },
-            en:{
-                title: '<span class="text-black">InterActive+ is recruiting!</span>',
-                description: '<span class="text-black">We are ready to welcome you in the year of 2019!</span>',
-                disableArticleButton: false,
-                customButton: false,
-                customButtonURL: null,
-                customButtonText: null,
-                articleIDRelated: 2
-            }
-        },
-        {
             backgroundImage: "static/slidesResources/slide1.jpg",
-            cn:{
-                title: '<span class="text-white">形随意动新官网上线!</span>',
-                description: '<span class="text-white">就在今天, 形随意动的新官网上线了, 让我们一起庆祝这一伟大时刻!</span>',
-                disableArticleButton: false,
-                customButton: false,
-                customButtonURL: null,
-                customButtonText: null,
-                articleIDRelated: 1
-            },
             en:{
                 title: '<span class="text-white">We launched our new website!</span>',
-                description: '<span class="text-white">Interactive+ launched its new website today, let\'s celebrate this glory moment!</span>',
+                description: '<span class="text-white">Darbots launched its new website today, let\'s celebrate this glory moment!</span>',
                 disableArticleButton: false,
                 customButton: false,
                 customButtonURL: null,
                 customButtonText: null,
-                articleIDRelated: 1
+                articleIDRelated: null
             }
         }
     ]
