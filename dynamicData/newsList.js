2267a90c2409cdfb767587ccf847d1de94f2a222
年迈的老秋风Windy <cxcyhdyx@sina.com>
2019-10-25 11:15:09 -0400

    Add two new videos.

--- dynamicData/newsList.js
+++ dynamicData/newsList.js
@@ -9,6 +9,26 @@ var XSYD_News = {
                 isURLAbsolute: false,
                 AuthorID: 0
             }
+        },
+        { //Second Element, articleID = 1
+            en:{
+                title: "Team 5100 Rover Ruckus Promo Video Released",
+                briefDescription: "Darbots revealed team 5100 Rover Ruckus promotion video on Youtube",
+                articleURL: "1_5100RoverRuckusPromo.html",
+                showOnIndexNews: true,
+                isURLAbsolute: false,
+                AuthorID: 3
+            }
+        },
+        { //articleID = 2
+            en:{
+                title: "SkyStone End-Of-October Autonomous Video Released",
+                briefDescription: "Darbots released team 4100 SkyStone Season End-Of-October Autonomous Video on Youtube",
+                articleURL: "2_SkyStoneEndOfOctAuto.html",
+                showOnIndexNews: true,
+                isURLAbsolute: false,
+                AuthorID: 3
+            }
         }
     ],
     articlesBaseURL: "dynamicData/articles/"
